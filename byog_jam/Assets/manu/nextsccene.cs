039d4cb3a0314bc90d19a6f59d689d33fd103a7a
aundet <klymenko144@gmail.com>
2023-09-17 09:42:25 +0200

    new folder

---
+++ byog_jam/Assets/manu/nextsccene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextsccene : MonoBehaviour
+{
+   
+    public void newscene() 
+    
+    {
+        SceneManager.LoadScene("");
+    
+    }
+}
