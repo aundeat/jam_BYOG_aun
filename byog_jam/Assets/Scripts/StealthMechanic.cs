63146824f8f5be6c800251990b933299689a9e0e
Oleh20 <oleh10836@gmail.com>
2023-09-17 12:21:08 +0200

    save

--- byog_jam/Assets/Scripts/StealthMechanic.cs
+++ byog_jam/Assets/Scripts/StealthMechanic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StealthMechanic : MonoBehaviour
@@ -9,18 +10,24 @@ public class StealthMechanic : MonoBehaviour
     public float maxVisibility = 100f; 
     public float visibilityIncreaseRate = 1f; 
     public float visibilityDecreaseRate = 0.5f;
+    private bool loser = false;
 
-    public float currentVisibility = 0f; 
-
+    public float currentVisibility = 0f;
+    [SerializeField] private GameObject lostText;
     private void Start()
     {
+        Time.timeScale = 1;
+        maxVisibility = 100f;
         currentVisibility = 0f;
         UpdateVisibilityUI();
     }
 
     private void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.R) && loser == true)
+        {
+            RestartScene();
+        }
         currentVisibility = Mathf.Clamp(currentVisibility, 0f, maxVisibility);
 
         UpdateVisibilityUI();
@@ -42,10 +49,24 @@ public class StealthMechanic : MonoBehaviour
         {
             visibilitySlider.value = currentVisibility / maxVisibility;
         }
+        if(visibilitySlider.value >= 0.9f)
+        {
+            lostText.SetActive(true);
+            loser = true;
+        }
     }
 
-    private void GameOver()
+    void RestartScene()
     {
-        Debug.Log("Loser");
+        Time.timeScale = 1;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+      
+    }
+    void GameOver()
+    {
+        lostText.SetActive(true);
+        loser = true;
+        Time.timeScale = 0;
     }
 }
